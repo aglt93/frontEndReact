c1946c4eb13a9d62a45c99bae21825c37c98c7fb
Alejandro Leon Torres <Alejandro.leon@nextest.com>
2019-07-09 18:32:14 -0600

    Assigment 1 part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import uniqueid from 'uniqid';
 
 
 class DishDetail extends Component {
@@ -17,7 +18,7 @@ class DishDetail extends Component {
         if(this.props.dish != null) {
         
             renderValue = <React.Fragment>
-                <div key={this.props.dish.id} className="col-12 col-md-5 m-1">
+                <div key={uniqueid()} className="col-12 col-md-5 m-1">
                     <Card>
                         <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
                         <CardBody>
@@ -26,13 +27,13 @@ class DishDetail extends Component {
                         </CardBody>
                     </Card>
                 </div>
-                <div key={this.props.dish.id} className="col-12 col-md-5 m-1">
+                <div key={uniqueid()} className="col-12 col-md-5 m-1">
                     <h2>Comments</h2>
                     <ul className="list-group">
                         {this.props.dish.comments.map((com) => {
-                            return(<React.Fragment>
-                                <li class="list-group-item">{com.author}, {com.date}</li>
-                                <li class="list-group-item">{com.comment}</li>
+                            return(<React.Fragment key={uniqueid()}>
+                                <li className="list-group-item">{com.author}, {com.date}</li>
+                                <li className="list-group-item">{com.comment}</li>
                             </React.Fragment>)
                         })}
                     </ul>
