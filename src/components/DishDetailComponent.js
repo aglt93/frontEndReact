2dd985882174dd3018eb8265ff13ec153afa7b08
Alejandro Leon Torres <Alejandro.leon@nextest.com>
2019-07-11 12:01:59 -0600

    Functional components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,71 +2,68 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import uniqueid from 'uniqid';
 
-
-class DishDetail extends Component {
     
-    renderDish(dish) {
+function RenderDish({dish}) {
 
-        const dishDetail = (
-            <div key={uniqueid()} className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
+    const dishDetail = (
+        <div key={uniqueid()} className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
 
-        return dishDetail;
-    }
+    return dishDetail;
+}
 
-    renderComments(comments) {
+function RenderComments({comments}) {
 
-        const commentsJSX = (
-            <div key={uniqueid()} className="col-12 col-md-5 m-1">
-                <h2>Comments</h2>
-                <ul className="list-unstyled">
-                    {comments.map((c) => {
-                        return(
-                            <React.Fragment key={uniqueid()}>
-                                <li className="p-2">{c.comment}</li>
-                                <li className="p-2">
-                                    --{c.author} {" , "} 
-                                    {new Intl.DateTimeFormat('en-US', 
-                                            { year: 'numeric', month: 'short', day: '2-digit' }
-                                        ).format(new Date(c.date))}
-                                </li>
-                            </React.Fragment>
-                        )
-                    })}
-                </ul>
-            </div>
-        );
+    const commentsJSX = (
+        <div key={uniqueid()} className="col-12 col-md-5 m-1">
+            <h2>Comments</h2>
+            <ul className="list-unstyled">
+                {comments.map((c) => {
+                    return(
+                        <React.Fragment key={uniqueid()}>
+                            <li className="p-2">{c.comment}</li>
+                            <li className="p-2">
+                                --{c.author} {" , "} 
+                                {new Intl.DateTimeFormat('en-US', 
+                                        { year: 'numeric', month: 'short', day: '2-digit' }
+                                    ).format(new Date(c.date))}
+                            </li>
+                        </React.Fragment>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 
-        return commentsJSX;
+    return commentsJSX;
 
-    }
+}
 
-    render() {
+const DishDetail = (props) => {
 
-        let renderValue = <div/>
+    let renderValue = <div/>
 
-        if(this.props.dish != null) {
-        
-            renderValue = (
-                <div className="container">
-                    <div className="row">
-                        {this.renderDish(this.props.dish)}
-                        {this.renderComments(this.props.dish.comments)}
-                    </div>
+    if(props.dish != null) {
+
+        renderValue = (
+            <div className="container">
+                <div className="row">
+                    <RenderDish dish={props.dish}/>
+                    <RenderComments comments={props.dish.comments}/>
                 </div>
-            );
-        }
-
-        return renderValue;
+            </div>
+        );
     }
+
+    return renderValue;
 }
 
 export default DishDetail;
